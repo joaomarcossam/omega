6a875bd803575ebe3e69a02fb56fb1e8ac1f78cc
joaomarcossam <joaomarcossandre@hotmail.com>
2025-07-30 01:40:43 -0300

    extra: adição de arquivos init, ajustes na main

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from bot.omegon import Omegon
+from utils.module import Module
+from utils.font import Font
+
 def main():
-    print("Hello from omega!")
+    #omegon = Omegon().start()
+    #omegon.run()
+    #Module.stop_all_modules()
+
+    print(Font("Teste").strikethrough)
 
 
 if __name__ == "__main__":
