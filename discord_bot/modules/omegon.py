8ff02bde8df482c6ce0ce619d735568206a8d940
Davidson-RF <davidson.frade@grupobyx.com.br>
2025-09-20 19:40:23 -0300

    bugfix: ajuste logs omegon.py

--- discord_bot/modules/omegon.py
+++ discord_bot/modules/omegon.py
@@ -1,21 +1,21 @@
 import discord
 from discord.ext import commands
-from utils.font import Font
 from utils.module import Module
+from logging import getLogger
+
+logger = getLogger("discord_logs")  # mesmo nome que já está configurado no projeto
 
 class Omegon(commands.Cog, Module):
     def __init__(self, bot):
         self.bot = bot
         super().__init__()
 
-    # (Opcional) Apenas manter o on_ready se quiser um "log" específico
     @commands.Cog.listener()
     async def on_ready(self):
-        print(Font("♪ Omegon module loaded!").cyan.double_underline.bold)
+        logger.info("♪ Omegon module loaded!")  # vai pro console + Discord se handler estiver ativo
 
-    # Comando para parar o bot
     @commands.command(name="stop")
     async def stop(self, ctx):
         await ctx.send("Bot is stopping...")
-        print(f"{Font('Stopping Program...').black.bold}")
+        logger.warning("Stopping Program...")
         Module.stop_all_modules()
