7f4fee08e7f0b4ca000de191710fd562fab6bc4f
Davidson-RF <davidson.frade@grupobyx.com.br>
2025-09-20 00:00:45 -0300

    feat: Ajuste main

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from discordia.omegon import Omegon
-# from utils.module import Module
+from environment import Env
 from utils.font import Font
 
-from environment import Env
-
-import sys
-
 def main():
-    print(type(Env))    
-    # omegon = Omegon().start()
-    # omegon.run()
-    # Module.stop_all_modules()
-    
+    Env.load()
+    print(Font("Booting Omegon...").cyan)
+    omegon = Omegon()
+    omegon.run()
 
 if __name__ == "__main__":
     main()
