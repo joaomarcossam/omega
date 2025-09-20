9da854514a0efca3c977202ff193183331eae564
Davidson-RF <davidson.frade@grupobyx.com.br>
2025-09-20 15:10:10 -0300

    feat: ajuste add Cogs

--- discord_bot/core/bot.py
+++ discord_bot/core/bot.py
@@ -1,23 +1,31 @@
-import discord
 from discord.ext import commands
 from ..core.settings import Env
 from utils.font import Font
+from ..modules.logchannel import LogChannel
 from ..modules.omegon import Omegon
 from ..modules.hello import Hello
 
 def create_bot():
-    intents = discord.Intents.default()
+    intents = commands.Intents.default()
     intents.message_content = True
     bot = commands.Bot(command_prefix="?", intents=intents)
-
-    # registrar cogs
-    bot.add_cog(Omegon(bot))
-    bot.add_cog(Hello(bot))
-
     return bot
+
+async def register_cogs(bot):
+    await bot.add_cog(Omegon(bot))
+    await bot.add_cog(Hello(bot))
+    await bot.add_cog(LogChannel(bot))
 
 def run():
     Env.load()
     bot = create_bot()
     print(Font("Omegon is starting...").blink.cyan)
-    bot.run(Env.OMEGON_TOKEN)
+
+    # usa asyncio.run para iniciar com setup assíncrono
+    import asyncio
+
+    async def runner():
+        await register_cogs(bot)
+        await bot.start(Env.OMEGON_TOKEN)
+
+    asyncio.run(runner())
