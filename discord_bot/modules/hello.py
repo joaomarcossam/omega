1422af9eadfbf83ef4bff72e7b75f544d957005e
Davidson-RF <davidson.frade@grupobyx.com.br>
2025-09-20 19:18:23 -0300

    feat: teste logging em Hello command

--- discord_bot/modules/hello.py
+++ discord_bot/modules/hello.py
@@ -1,4 +1,7 @@
 from discord.ext import commands
+import logging
+
+logger = logging.getLogger("discord_logs")
 
 class Hello(commands.Cog):
     def __init__(self, bot):
@@ -6,4 +9,5 @@ class Hello(commands.Cog):
 
     @commands.command(name="hello")
     async def hello(self, ctx):
+        logging.info("Hello command called by %s", ctx.author)
         await ctx.send("Hello! 👋")
